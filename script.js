ea832a52a67bb8045a8bac20b9ce275fdff4e81a
Yehiashamel <102812060+Yehiashamel@users.noreply.github.com>
2022-07-14 19:10:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let menuBtn = document.querySelector('#nav-btn');
 let nav = document.querySelector('#ul');
+let icon = document.querySelector('#icon');
+
 menuBtn.onclick = function(){
     nav.classList.toggle("active");
+    icon.classList.toggle("fa-xmark");
 }
 document.body.onscroll = () =>{
     nav.classList.remove("active")
